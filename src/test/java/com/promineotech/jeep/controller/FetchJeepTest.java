2977b33f158464976b65e6f8dd2e27228f9543f5
Jovan <avannr@gmail.com>
2022-12-13 23:31:52 -0700

    Week 14

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,6 +1,8 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +37,37 @@ class FetchJeepTest {
     JeepModel model = JeepModel.WRANGLER;
     String trim = "Sport";
     String uri = String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
-    ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+    ResponseEntity<List<Jeep>> response = restTemplate.exchange
+        (uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
+    List<Jeep> expected = buildExpected();
+    System.out.println(expected);
+    assertThat(response.getBody()).isEqualTo(expected);
+    }
   
+ protected List<Jeep> buildExpected(){
+   List<Jeep> list = new LinkedList<>();
+   
+   //@formatter:off
+   list.add(Jeep.builder()
+       .modelId(JeepModel.WRANGLER)
+       .trimLevel("Sport")
+       .numDoors(2)
+       .wheelSize(17)
+       .basePrice(new BigDecimal("28475.00"))
+       .build());
+   
+   list.add(Jeep.builder()
+       .modelId(JeepModel.WRANGLER)
+       .trimLevel("Sport")
+       .numDoors(4)
+       .wheelSize(17)
+       .basePrice(new BigDecimal("31975.00"))
+       .build());
+   //@formatter:on
+   
+   return list;
+  }
   @Autowired
   private TestRestTemplate restTemplate;
   
